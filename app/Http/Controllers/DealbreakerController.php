0ba3144cd75eebf2b8d564d75db7b8abd0c47ec6
Kate Barrera <kate@kodakollectiv.com>
2023-09-13 16:02:52 +0800

    feat: save dealbreaker choices

--- app/Http/Controllers/DealbreakerController.php
+++ app/Http/Controllers/DealbreakerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Dealbreaker;
+use App\Models\DealbreakerChoice;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -28,6 +29,17 @@ class DealbreakerController extends Controller
             'question_type' => $request['question_type'],
             'company_id' => $request['company_id'],
         ]);
+
+        if ($request->filled('choices')) {
+            $dealbreakers = explode(',', $request['choices']);
+            // foreach ($request['choices'] as $choices) {
+            for ($i = 0; $i < sizeof($$dealbreakers); $i++) {
+                DealbreakerChoice::create([
+                    'dealbreaker_id' => $dealbreaker->id,
+                    'choice' => $i,
+                ]);
+            }
+        }
 
         return response([
             'dealbreaker' => $dealbreaker,
