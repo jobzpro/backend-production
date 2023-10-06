7ac80b2cf2585ddc22047e6ad93fa84e57696d06
Chervy Cutie <chervin.tanilon@kodakollectiv.com>
2023-10-06 11:47:22 +0800

    fix: null error in applicant application email to employer

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -134,21 +134,19 @@ class JobApplicationController extends Controller
             'is_Read' => false,
         ]);
 
-        return response()->json($user_companies, 200);
-
         if ($user_companies) {
             foreach ($user_companies as $employer) {
-                return (new EmployerMailerController)->applicantApplied($user, $employer, $company, $job_list);
+                (new EmployerMailerController)->applicantApplied($user, $employer, $company, $job_list);
             }
         }
 
-        // if ($user) {
-        //     (new MailerController)->sendApplicationSuccess($user, $company, $job_list);
-        // }
+        if ($user) {
+            (new MailerController)->sendApplicationSuccess($user, $company, $job_list);
+        }
 
-        // return response([
-        //     'message' => 'Application Successfully Submitted',
-        // ], 200);
+        return response([
+            'message' => 'Application Successfully Submitted',
+        ], 200);
     }
 
     /**
