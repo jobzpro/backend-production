47e303ea101ec7b4914a1da71c905001eadd0394
Kate Barrera <kate@kodakollectiv.com>
2023-10-06 14:23:11 +0800

    feat: create api for company reviews

--- app/Http/Controllers/CompanyReviewController.php
+++ app/Http/Controllers/CompanyReviewController.php
@@ -12,6 +12,20 @@ class CompanyReviewController extends Controller
         $company = Company::find($id);
 
         if ($company) {
+            $company->companyReviews()->create([
+                'user_id' => $request['user_id'],
+                'company_id' => $company->id,
+                'rating' => $request['rating'],
+                'currently_employed' => $request['currently_employed'],
+                'employment_status' => $request['employment_status'],
+                'position' => $request['position'],
+                'content' => $request['content'],
+            ]);
+
+            return response([
+                'reviews' => $company->companyReviews,
+                'message' => "Review added successfully."
+            ], 200);
         } else {
             return response([
                 'message' => 'Not found',
