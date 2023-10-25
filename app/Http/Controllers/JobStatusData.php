dabfd8d0d479336a1409c90bef795e0fe0d647cd
Marlou Pepito <marlou.pepito@kodakollectiv.com>
2023-10-25 12:10:34 +0800

    added api job status data in api and create JobStatusData controller

---
+++ app/Http/Controllers/JobStatusData.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\JobList;
+use App\Models\JobApplication;
+use App\Models\User;
+use App\Models\Company;
+use App\Models\AppReview;
+use App\Models\CompanyReview;
+use App\Models\Industry;
+
+class JobStatusData extends Controller
+{
+    public function get_job_status_data(){
+        $job_posting = JobList::count();
+        $hired = JobApplication::where('status','hired')->count();;
+        $employers = Company::count();
+        $users = User::count();
+
+        return response([
+            'job_posting' => $job_posting,
+            'hired'=>$hired,
+            'employers' =>$employers,
+            'users'=>$users,
+            'message' => "Success",
+        ], 200);
+    }
+    
+    public function get_trending_categories() {
+        $industry = Industry::get();
+        
+        $industry->each(function ($industry) {
+            $countData = CompanyReview::where('company_id', $industry->company_id)->count();
+            $industry->count = $countData??0;
+        });
+        
+        return response([
+            'industry' =>$industry,
+            'message' => "Success",
+        ], 200);
+    }
+    
+    public function get_top_rated_companies() {
+        $company = Company::limit(9)->get();
+
+        $company->each(function ($company) {
+            $averageRating = CompanyReview::where('company_id', $company->id)->avg('rating');
+            $company->average_rating = $averageRating??0;
+        });
+
+        return response([
+            'company' => $company,
+            'message' => "Success",
+        ], 200);
+    }
+
+    public function get_users_review() {
+       $reviews =  AppReview::where('pin',true)->limit(9)->get();
+        return response([
+            'reviews'=> $reviews,
+            'message' => "Success",
+        ], 200);
+    }
+}
