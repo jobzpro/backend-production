edc611bb6f5695f0af03e9ac0c5bc222bda004e3
Ferjade <ferjade.fortuito@kodakollectiv.com>
2023-11-24 17:06:00 +0800

    fix: interview_date throwing error for set-interviews

--- app/Http/Controllers/JobInterviewController.php
+++ app/Http/Controllers/JobInterviewController.php
@@ -82,7 +82,7 @@ class JobInterviewController extends Controller
             'company_id' => $company->id,
             'notes' => $data['notes'],
             'meeting_link' => $data['meeting_link'],
-            'interview_date' => Carbon::parse($data['interview_date'])->format("d/m/Y h:m A"),
+            'interview_date' => Carbon::parse($data['interview_date'])->format("d/m/Y h:m"),
         ]);
 
         $jobApplication->update([
