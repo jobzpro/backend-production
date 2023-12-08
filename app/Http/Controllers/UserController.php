d88b8e17c46ba81e8be06bbaf6f7f44b2244052f
JesseOddJobs <jesse.basco@kodakollectiv.com>
2023-12-08 16:40:24 +0800

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -301,7 +301,7 @@ class UserController extends Controller
         $keyword = $request->query('keyword');
         $sortFilter = $request->query('sort');
 
-        $users = User::all()->orderBy('profile_completion', 'desc');
+        $users = User::orderBy('profile_completion', 'desc');
         // $users = User::orderBy('id', 'ASC');
 
         // if (!empty($keyword)) {
