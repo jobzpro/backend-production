39198cba581a22d5fa3e28f4d7d489f01036ad2d
JesseOddJobs <jesse.basco@kodakollectiv.com>
2023-12-22 10:28:27 +0800

    Fix: Search Keyword filter in JobApplication

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -37,22 +37,30 @@ class JobApplicationController extends Controller
         // $applications = JobApplication::with('jobList', 'jobInterviews', 'user');
         $user = User::where('account_id', Auth::id())->first();
         $applications = JobApplication::with('jobList', 'jobInterviews', 'user', 'jobList.experience_level', 'user.user_experience')
-                ->whereHas('jobList', function ($q) use ($company_id){
+                ->whereHas('jobList', function ($q) use ($company_id, $keyword){
                     $q->where('company_id', $company_id);
+                    if(!$keyword == null){
+                        $q->where('job_title', 'LIKE', '%' . $keyword . '%')
+                        ->whereHas('user', function($y) use($keyword){
+                            $y->where('first_name', 'LIKE', '%' . $keyword . '%')
+                            ->orWhere('middle_name', 'LIKE', '%' . $keyword . '%')
+                            ->orWhere('last_name', 'LIKE', '%' . $keyword . '%');
+                        });
+                    }
                 })
                 ->orderBy('created_at', $order);
 
-        if(!$keyword == null) {
-            $applications = $applications
-            ->whereHas('jobList', function ($query) use ($keyword){
-                $query->where('job_title', 'LIKE', '%' . $keyword . '%');
-            })
-            ->orWhereHas('user', function ($query) use ($keyword){
-                $query->where('first_name', 'LIKE', '%' . $keyword . '%')
-                ->orWhere('middle_name', 'LIKE', '%', '%' . $keyword . '%')
-                ->orWhere('last_name', 'LIKE', '%', '%' . $keyword . '%');
-            });
-        }
+        // if(!$keyword == null) {
+        //     $applications = $applications
+        //     ->whereHas('jobList', function ($query) use ($keyword){
+        //         $query->where('job_title', 'LIKE', '%' . $keyword . '%');
+        //     })
+        //     ->orWhereHas('user', function ($query) use ($keyword){
+        //         $query->where('first_name', 'LIKE', '%' . $keyword . '%')
+        //         ->orWhere('middle_name', 'LIKE', '%', '%' . $keyword . '%')
+        //         ->orWhere('last_name', 'LIKE', '%', '%' . $keyword . '%');
+        //     });
+        // }
 
         if (!$sortFilter == null) {
             if ($sortFilter == "Recent to Oldest") {
