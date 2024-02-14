4ec196784a6d0fa790fec8abbf6e2d1930290352
Aubrey <aubrey@kodakollectiv.com>
2024-02-14 15:33:23 +0800

    Revert "test: dealbreaker and job list fix"
    This reverts commit 02d189f68fa1586f97b7b2e3a8c2cca7811d4b8d.

--- app/Models/JobListDealbreaker.php
+++ app/Models/JobListDealbreaker.php
@@ -25,7 +25,7 @@ class JobListDealbreaker extends Model
 
     public function jobList(): BelongsTo
     {
-        return $this->belongsTo(JobList::class);
+        return $this->belongsTo(JobList::class, 'job_list_id');
     }
 
     public function dealbreaker(): BelongsTo
