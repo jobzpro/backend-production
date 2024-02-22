d6022788830241c701e4cd1ce092d415631d5ca0
Aubrey <aubrey@kodakollectiv.com>
2024-02-22 14:49:53 +0800

    revert changes

--- app/Http/Controllers/JobListDealbreakerController.php
+++ app/Http/Controllers/JobListDealbreakerController.php
@@ -9,26 +9,25 @@ class JobListDealbreakerController extends Controller
 {
     public function softDeleteDealbreakerAnswer(Request $request)
     {
-        dd($request->input('job_list_id'));
-        // if ($request->input('job_list_id')) {
-        //     $jLDealbreaker = JobListDealbreaker::where('job_list_id', '=', $request->input('job_list_id'));
-        //     $count = $jLDealbreaker->count();
-        //     dd($jLDealbreaker->get());
-        //     // if ($count > 0) {
-        //     //     $jLDealbreaker->forceDelete();
-        //     //     return response()->json([
-        //     //         'message' => 'success',
-        //     //         'count' => $jLDealbreaker
-        //     //     ], 200);
-        //     // } else {
-        //     //     return response()->json([
-        //     //         'message' => 'No records found for soft deletion',
-        //     //     ], 400);
-        //     // }
-        // } else {
-        //     return response([
-        //         'message' => "job list id is missing, try again"
-        //     ], 500);
-        // }
+        if ($request->input('job_list_id')) {
+            $jLDealbreaker = JobListDealbreaker::where('job_list_id', '=', $request->input('job_list_id'));
+            $count = $jLDealbreaker->count();
+            dd($jLDealbreaker->get());
+            // if ($count > 0) {
+            //     $jLDealbreaker->forceDelete();
+            //     return response()->json([
+            //         'message' => 'success',
+            //         'count' => $jLDealbreaker
+            //     ], 200);
+            // } else {
+            //     return response()->json([
+            //         'message' => 'No records found for soft deletion',
+            //     ], 400);
+            // }
+        } else {
+            return response([
+                'message' => "job list id is missing, try again"
+            ], 500);
+        }
     }
 }
