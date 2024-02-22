44f31bcae7a71a798b94320ec3db78099536d7e5
Aubrey <aubrey@kodakollectiv.com>
2024-02-22 14:53:30 +0800

    soft Deletetion

--- app/Http/Controllers/JobListDealbreakerController.php
+++ app/Http/Controllers/JobListDealbreakerController.php
@@ -12,18 +12,17 @@ class JobListDealbreakerController extends Controller
         if ($request->input('job_list_id')) {
             $jLDealbreaker = JobListDealbreaker::where('job_list_id', '=', $request->input('job_list_id'));
             $count = $jLDealbreaker->count();
-            dd($jLDealbreaker->get());
-            // if ($count > 0) {
-            //     $jLDealbreaker->forceDelete();
-            //     return response()->json([
-            //         'message' => 'success',
-            //         'count' => $jLDealbreaker
-            //     ], 200);
-            // } else {
-            //     return response()->json([
-            //         'message' => 'No records found for soft deletion',
-            //     ], 400);
-            // }
+            if ($count > 0) {
+                $jLDealbreaker->forceDelete();
+                return response()->json([
+                    'message' => 'success',
+                    'count' => $jLDealbreaker
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'No records found for soft deletion',
+                ], 400);
+            }
         } else {
             return response([
                 'message' => "job list id is missing, try again"
