b97962660fc067e09c07f94add93521df29e0733
Aubrey <aubrey@kodakollectiv.com>
2024-02-22 14:25:41 +0800

    add dd for debugging purpose

--- app/Http/Controllers/JobListDealbreakerController.php
+++ app/Http/Controllers/JobListDealbreakerController.php
@@ -11,17 +11,18 @@ class JobListDealbreakerController extends Controller
         if ($job_list_id) {
             $jLDealbreaker = JobListDealbreaker::where('job_list_id', '=', $job_list_id);
             $count = $jLDealbreaker->count();
-            if ($count > 0) {
-                $jLDealbreaker->forceDelete();
-                return response()->json([
-                    'message' => 'success',
-                    'count' => $jLDealbreaker
-                ], 200);
-            } else {
-                return response()->json([
-                    'message' => 'No records found for soft deletion',
-                ], 400);
-            }
+            dd($jLDealbreaker->get());
+            // if ($count > 0) {
+            //     $jLDealbreaker->forceDelete();
+            //     return response()->json([
+            //         'message' => 'success',
+            //         'count' => $jLDealbreaker
+            //     ], 200);
+            // } else {
+            //     return response()->json([
+            //         'message' => 'No records found for soft deletion',
+            //     ], 400);
+            // }
         } else {
             return response([
                 'message' => "job list id is missing, try again"
