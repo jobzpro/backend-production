3c4296ad42a582ef40cdafab9a618537b5d9bdda
Aubrey <aubrey@kodakollectiv.com>
2024-02-27 15:42:55 +0800

    comment out add_photo issue

--- database/migrations/2024_02_27_061705_add_photo_to_notifications_table.php
+++ database/migrations/2024_02_27_061705_add_photo_to_notifications_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('notifications', function (Blueprint $table) {
-            $table->string('photo')->nullable();
-            $table->integer('user_id')->nullable();
-            $table->integer('company_id')->nullable();
+            // $table->string('photo')->nullable();
+            // $table->integer('user_id')->nullable();
+            // $table->integer('company_id')->nullable();
         });
     }
 
