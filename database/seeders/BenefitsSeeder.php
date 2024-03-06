10e63609684249fb890bdfc82654917c37d0a841
Aubrey <aubrey@kodakollectiv.com>
2024-03-06 13:32:20 +0800

    change compensation seeder

--- database/seeders/BenefitsSeeder.php
+++ database/seeders/BenefitsSeeder.php
@@ -13,7 +13,7 @@ class BenefitsSeeder extends Seeder
      */
     public function run(): void
     {
-        $benefits = ["Health Insurance", "Gym Membership", "Dental Insurance", "Vision Insurance", "Paid Time Off", "Life Insurance", "401(k) matching", "Flexible Schedule", "Tuition Reimbursement", "Disability Insurance", "Continuing Education Credits", "Flexible Schedule", "Referral Program", "Employee Assistance", "Flexible Spending Account", "Free Parking", "Travel Reimbursement", "Malpractice Insurance", "Paid Sick Time", "Employee Discount", "Health Savings Account", "Paid Training", "Other"];
+        $benefits = ["Health Insurance", "Gym Membership", "Dental Insurance", "Vision Insurance", "Paid Time Off", "Life Insurance", "401(k) matching", "Flexible Schedule", "Tuition Reimbursement", "Disability Insurance", "Continuing Education Credits", "Mental Health Therapy", "Referral Program", "Employee Assistance", "Flexible Spending Account", "Free Parking", "Travel Reimbursement", "Malpractice Insurance", "Paid Sick Time", "Employee Discount", "Health Savings Account", "Paid Training", "Other"];
 
 
         for ($i = 0; $i < sizeof($benefits); $i++) {
