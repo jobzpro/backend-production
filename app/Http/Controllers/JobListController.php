14404a263e3459bb77256e5f1ad0d190b0becd6f
Aubrey <aubrey@kodakollectiv.com>
2024-03-22 10:30:05 +0800

    debuging

--- app/Http/Controllers/JobListController.php
+++ app/Http/Controllers/JobListController.php
@@ -932,17 +932,22 @@ class JobListController extends Controller
             // $date_selected = Carbon::parse($date);
             $date_selected = new Carbon($date);
             $date_now = Carbon::now();
-            $job_lists = JobList::whereBetween('created_at', [$date_now->format('Y-m-d'),  $date_selected->format('Y-m-d')])
-                // whereDate('created_at', '<=', $date_selected->format('Y-m-d'))
-                //     ->whereDate('created_at', '>=', $date_now->format('Y-m-d'))
-                ->with('company', 'industry', 'job_location', 'job_types.type', 'job_benefits.benefits', 'job_specialities.industrySpeciality', 'jobListDealbreakers.dealbreaker.choices')
-                ->orderBy('updated_at', 'DESC')
-                ->get();
-
-            return response([
-                'job_lists' => $job_lists->paginate(10),
-                'message' => "Success",
-            ], 200);
+            // $job_lists = JobList::whereBetween('created_at', [
+            //     $date_now->format('Y-m-d'),
+            //     $date_selected->format('Y-m-d')
+            // ])
+            //     ->with('company', 'industry', 'job_location', 'job_types.type', 'job_benefits.benefits', 'job_specialities.industrySpeciality', 'jobListDealbreakers.dealbreaker.choices')
+            //     ->orderBy('updated_at', 'DESC')
+            //     ->get();
+            $job_lists = JobList::whereBetween('created_at', [
+                $date_now->format('Y-m-d'),
+                $date_selected->format('Y-m-d')
+            ])->toSql();
+            dd($job_lists);
+            // return response([
+            //     'job_lists' => $job_lists->paginate(10),
+            //     'message' => "Success",
+            // ], 200);
         } elseif (!($keyword == null && $location == null && $industry == null && $job_type == null && $qualifications == null && $date == null)) {
             // $date_selected = Carbon::parse($date);
             $date_selected = new Carbon($date);
