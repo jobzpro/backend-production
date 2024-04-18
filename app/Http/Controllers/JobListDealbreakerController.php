197bbfa01860e7b6ef6a317b3206698298be4d19
Aubrey <aubrey@kodakollectiv.com>
2024-04-18 16:00:20 +0800

    merge validation jobdealbreaker answer

--- app/Http/Controllers/JobListDealbreakerController.php
+++ app/Http/Controllers/JobListDealbreakerController.php
@@ -22,6 +22,20 @@ class JobListDealbreakerController extends Controller
                     'message' => 'No records found for soft deletion',
                 ], 400);
             }
+        } else if ($request->input('job_list_id') && $request->input('dealbreaker_id')) {
+            $jLDealbreaker = JobListDealbreaker::where('job_list_id', '=', $request->input('job_list_id'))
+                ->where('dealbreaker_id', '=', $request->input('dealbreaker_id'));
+            $count = $jLDealbreaker->count();
+            if ($count > 0) {
+                $jLDealbreaker->forceDelete();
+                return response()->json([
+                    'message' => 'success',
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'No records found for soft deletion',
+                ], 400);
+            }
         } else {
             return response([
                 'message' => "job list id is missing, try again"
