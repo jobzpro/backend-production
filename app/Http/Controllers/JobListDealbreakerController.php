77a0da545f6a5f2e3d8722f7def35882a0d53fc0
Aubrey <aubrey@kodakollectiv.com>
2024-04-22 07:59:01 +0800

    comment out joblist dealbreakers delete

--- app/Http/Controllers/JobListDealbreakerController.php
+++ app/Http/Controllers/JobListDealbreakerController.php
@@ -22,20 +22,6 @@ class JobListDealbreakerController extends Controller
                     'message' => 'No records found for soft deletion',
                 ], 400);
             }
-        } else if ($request->input('job_list_id') && $request->input('dealbreaker_id')) {
-            $jLDealbreaker = JobListDealbreaker::where('job_list_id', '=', $request->input('job_list_id'))
-                ->where('dealbreaker_id', '=', $request->input('dealbreaker_id'));
-            $count = $jLDealbreaker->count();
-            if ($count > 0) {
-                $jLDealbreaker->forceDelete();
-                return response()->json([
-                    'message' => 'success',
-                ], 200);
-            } else {
-                return response()->json([
-                    'message' => 'No records found for soft deletion',
-                ], 400);
-            }
         } else {
             return response([
                 'message' => "job list id is missing, try again"
@@ -43,26 +29,25 @@ class JobListDealbreakerController extends Controller
         }
     }
 
-    public function selectForceDeleteAnswer(Request $request)
-    {
-        $jLDealbreaker = JobListDealbreaker::where('job_list_id', '=', $request->input('job_list_id'))
-            ->where('dealbreaker_id', '=', $request->input('dealbreaker_id'));
-        $count = $jLDealbreaker->count();
-        if ($request->input('job_list_id') && $request->input('dealbreaker_id')) {
-            if ($count > 0) {
-                $jLDealbreaker->forceDelete();
-                return response()->json([
-                    'message' => 'success',
-                ], 200);
-            } else {
-                return response()->json([
-                    'message' => 'No records found for soft deletion',
-                ], 400);
-            }
-        } else {
-            return response([
-                'message' => "job list id is missing, try again"
-            ], 500);
-        }
-    }
+    //     public function selectForceDeleteAnswer(Request $request)
+    //     {
+    //         $jLDealbreaker = JobListDealbreaker::find($request->input('job_list_id'));
+    //         $count = $jLDealbreaker->count();
+    //         if ($request->input('job_list_id') && $request->input('dealbreaker_id')) {
+    //             if ($count > 0) {
+    //                 $jLDealbreaker->forceDelete();
+    //                 return response()->json([
+    //                     'message' => 'success',
+    //                 ], 200);
+    //             } else {
+    //                 return response()->json([
+    //                     'message' => 'No records found for soft deletion',
+    //                 ], 400);
+    //             }
+    //         } else {
+    //             return response([
+    //                 'message' => "job list id is missing, try again"
+    //             ], 500);
+    //         }
+    //     }
 }
