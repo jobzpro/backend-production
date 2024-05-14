17d79db3ec82d9ea1a2b96895fcf143438c7f43d
aubrey-yu-koda <aubrey@kodakollectiv.com>
2024-05-14 14:35:25 +0800

    follwoer user issue fix

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -133,11 +133,11 @@ class FollowerController extends Controller
             ], 200);
         } else if ($filter == "following") {
             $following = Follower::where('following_id', $id);
-            $followingUser = $following->with('followingUser');
+            $followingUser = $following->with('followerUser');
             // $followingUser = $following::with('followingUser.experiences', 'followingUser.certifications', 'followingUser.account', 'followingUser.references');
 
             if (!empty($keyword)) {
-                $followingUser->whereHas('followingUser', function ($query) use ($keyword) {
+                $followingUser->whereHas('followerUser', function ($query) use ($keyword) {
                     $query->where('first_name', 'LIKE', '%' . $keyword . '%')
                         ->orWhere('last_name', 'LIKE', '%' . $keyword . '%')
                         ->orWhereHas('currentExperience', function ($q) use ($keyword) {
@@ -145,7 +145,7 @@ class FollowerController extends Controller
                         });
                 });
             }
-            $followingUser->whereHas('followingUser.userRoles', function ($q) {
+            $followingUser->whereHas('followerUser.userRoles', function ($q) {
                 $q->where('role_id', 3);
             });
 
