d592a3eddd840b9c41e44d02c2691541987eeb34
aubrey-yu-koda <aubrey@kodakollectiv.com>
2024-05-21 16:51:58 +0800

    refactor payment link display

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -28,33 +28,23 @@ class ProductController extends Controller
         $stripe = new StripeClient(env('STRIPE_SECRET'));
 
         try {
-            // $allProducts = $stripe->products->all();
-            // $productsWithPrices = collect($allProducts->data)->filter(function ($product) {
-            //     return $product->active && $product->unit_label === 'jobseeker';
-            // })->map(function ($product) use ($stripe) {
-            //     $prices = $stripe->prices->all(['product' => $product->id]);
-            //     $payment_link = $stripe->paymentLinks->all(['product' => $product->id]);
-            //     $price = $prices->data[0]->unit_amount_decimal;
+            $allProducts = $stripe->products->all();
+            $productsWithPrices = collect($allProducts->data)->filter(function ($product) {
+                return $product->active && $product->unit_label === 'jobseeker';
+            })->map(function ($product) use ($stripe) {
+                $prices = $stripe->prices->all(['product' => $product->id]);
+                $payment_link = $stripe->paymentLinks->all(['product' => $product->id]);
+                $price = $prices->data[0]->unit_amount_decimal;
 
-            //     return [
-            //         // 'name' => $product->name,
-            //         // 'description' => $product->description,
-            //         // 'price' => $price,
-            //         'product' => $product,
-            //         'prices' => $prices,
-            //         'payment_link' => $payment_link,
-            //     ];
-            // });
+                return [
+                    'name' => $product->name,
+                    'description' => $product->description,
+                    'price' => $price,
+                    'payment_link' => isset($payment_link->data[0]) ? $payment_link->data[0]->url : null,
+                ];
+            });
 
-            $allProducts = $stripe->paymentLink->all();
-            $line_items = $allProducts->line_items;
-            $productsWithPrices = $allProducts->line_items->data;
-
-            return response([
-                'allProducts' => $allProducts,
-                'line_items' => $line_items,
-                'productsWithPrices' => $productsWithPrices
-            ], 200);
+            return response($productsWithPrices, 200);
         } catch (\Exception $e) {
             // Handle errors
             return response([
