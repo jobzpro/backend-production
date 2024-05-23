0fa16b635281e96968ad23cdff9e6de8dda579b9
aubrey-yu-koda <aubrey@kodakollectiv.com>
2024-05-23 08:08:42 +0800

    add product plan

--- app/Nova/ProductPlan.php
+++ app/Nova/ProductPlan.php
@@ -2,21 +2,22 @@
 
 namespace App\Nova;
 
-use App\Models\ProductPlan;
 use Illuminate\Http\Request;
-use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
-class Product extends Resource
+class ProductPlan extends Resource
 {
     /**
      * The model the resource corresponds to.
      *
-     * @var class-string<\App\Models\Product>
+     * @var class-string<\App\Models\ProductPlan>
      */
-    public static $model = \App\Models\Product::class;
+    public static $model = \App\Models\ProductPlan::class;
 
     /**
      * The single value that should be used to represent the resource when being displayed.
@@ -44,9 +45,18 @@ class Product extends Resource
     {
         return [
             ID::make()->sortable(),
-            Text::make("Code", "product_code"),
-            Text::make("Name", "name"),
-            BelongsTo::make('Plan', 'product_plans', ProductPlan::class),
+            Text::make("Price", "price"),
+            Select::make("Recurring", "recurring")->options([
+                'monthly' => 'Monthly',
+                'yearly' => 'Yearly',
+            ]),
+            // Text::make("Mode", "mode"),
+            Text::make("Unit Label", "unit_label"),
+            Text::make("Tag", "lookup_key"),
+            Textarea::make("Link", "checkout_url"),
+            Number::make("Connection Count", "connection_count"),
+            Number::make("Post Count", "post_count"),
+            Number::make("Applicant Count", "applicant_count"),
         ];
     }
 
