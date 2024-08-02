537fa7f5d26b62250e1f5a036b21f6819305d355
aubrey-yu-koda <aubrey@kodakollectiv.com>
2024-08-02 15:30:33 +0800

    product seeder for production

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -19,12 +19,14 @@ class ProductSeeder extends Seeder
         // jobseeker subscription
 
         $jobseeker9connections = Product::create([
-            "product_code" => "prod_QGSccjWbRQR4DZ",
+            // "product_code" => "prod_QGSccjWbRQR4DZ", //staging
+            "product_code" => "prod_QaPlBr2AmctuDI",
             "name" => "1-9 connections",
             "description" => "Up to 9 connections",
         ]);
         $jobseeker9connections->product_plans()->create([
-            'price_code' => 'price_1PPvhLChe3vlhgX37yYy1cDE',
+            // 'price_code' => 'price_1PPvhLChe3vlhgX37yYy1cDE', //staging
+            'price_code' => 'price_1PjEwGHxBaMvMKXwRfVvsBKu',
             'price' => 1.00,
             'recurring' => "month",
             "mode" => "subscription",
@@ -35,12 +37,14 @@ class ProductSeeder extends Seeder
         ]);
 
         $jobseeker10connections = Product::create([
-            "product_code" => "prod_QGSdKsyCcIdTO8",
+            // "product_code" => "prod_QGSdKsyCcIdTO8",
+            "product_code" => "prod_QaPoKCzTMidvlZ",
             "name" => "10 connections",
             "description" => "Up to 10 connections",
         ]);
         $jobseeker10connections->product_plans()->create([
-            'price_code' => 'price_1PPviGChe3vlhgX33iZh2Z7F',
+            // 'price_code' => 'price_1PPviGChe3vlhgX33iZh2Z7F',
+            'price_code' => 'price_1PjEypHxBaMvMKXwUkRikuMK',
             'price' => 1.50,
             'recurring' => "month",
             "mode" => "subscription",
@@ -51,12 +55,14 @@ class ProductSeeder extends Seeder
         ]);
 
         $jobseeker20connections = Product::create([
-            "product_code" => "prod_QGSeFsLBfKsonn",
+            // "product_code" => "prod_QGSeFsLBfKsonn",
+            "product_code" => "prod_QaPpH5aY391sxq",
             "name" => "20 connections",
             "description" => "Up to 20 connections",
         ]);
         $jobseeker20connections->product_plans()->create([
-            'price_code' => 'price_1PPvjOChe3vlhgX3cnT1DwDU',
+            // 'price_code' => 'price_1PPvjOChe3vlhgX3cnT1DwDU',
+            'price_code' => 'price_1PjF0KHxBaMvMKXwKFtIODmx',
             'price' => 3.00,
             'recurring' => "month",
             "mode" => "subscription",
@@ -67,12 +73,14 @@ class ProductSeeder extends Seeder
         ]);
 
         $jobseeker40connections = Product::create([
-            "product_code" => "prod_QGSfTaOjDONstL",
+            // "product_code" => "prod_QGSfTaOjDONstL",
+            "product_code" => "prod_QaPrK4ry2Quz4t",
             "name" => "40 connections",
             "description" => "Up to 40 connections",
         ]);
         $jobseeker40connections->product_plans()->create([
-            'price_code' => 'price_1PPvkDChe3vlhgX3tOE37Lqk',
+            // 'price_code' => 'price_1PPvkDChe3vlhgX3tOE37Lqk',
+            'price_code' => 'price_1PjF1aHxBaMvMKXw6Jge7igS',
             'price' => 6.00,
             'recurring' => "month",
             "mode" => "subscription",
@@ -83,12 +91,14 @@ class ProductSeeder extends Seeder
         ]);
 
         $jobseeker60connections = Product::create([
-            "product_code" => "prod_QGSgsW5TLTuoro",
+            // "product_code" => "prod_QGSgsW5TLTuoro",
+            "product_code" => "prod_QaPryUzGM5PR0V",
             "name" => "40 connections",
             "description" => "Up to 40 connections",
         ]);
         $jobseeker60connections->product_plans()->create([
-            'price_code' => 'price_1PPvlIChe3vlhgX3iCelrsES',
+            // 'price_code' => 'price_1PPvlIChe3vlhgX3iCelrsES',
+            'price_code' => 'price_1PjF2AHxBaMvMKXwdYc5xXHS',
             'price' => 9.00,
             'recurring' => "month",
             "mode" => "subscription",
@@ -99,12 +109,14 @@ class ProductSeeder extends Seeder
         ]);
 
         $jobseeker80connections = Product::create([
-            "product_code" => "prod_QGShTcip1o6LyJ",
+            // "product_code" => "prod_QGShTcip1o6LyJ",
+            "product_code" => "prod_QaPupgpgngmL95",
             "name" => "80 connections",
             "description" => "Up to 80 connections",
         ]);
         $jobseeker80connections->product_plans()->create([
-            'price_code' => 'price_1PPvm6Che3vlhgX3npDHj6Q0',
+            // 'price_code' => 'price_1PPvm6Che3vlhgX3npDHj6Q0',
+            'price_code' => 'price_1PjF4YHxBaMvMKXwiO4BxaOW',
             'price' => 12.00,
             'recurring' => "month",
             "mode" => "subscription",
@@ -115,12 +127,14 @@ class ProductSeeder extends Seeder
         ]);
 
         $jobseeker100connections = Product::create([
-            "product_code" => "prod_QGShJedsVJD3Rn",
+            // "product_code" => "prod_QGShJedsVJD3Rn",
+            "product_code" => "prod_QaPufdp5MbjKC9",
             "name" => "100 connections",
             "description" => "Up to 100 connections",
         ]);
         $jobseeker100connections->product_plans()->create([
-            "price_code" => "price_1PPvmVChe3vlhgX3JigrDcim",
+            // "price_code" => "price_1PPvmVChe3vlhgX3JigrDcim",
+            "price_code" => "price_1PjF55HxBaMvMKXwu2HkO43p",
             "price" => 15.00,
             "recurring" => "month",
             "mode" => "subscription",
@@ -130,12 +144,14 @@ class ProductSeeder extends Seeder
         ]);
 
         $jobseeker150connections = Product::create([
-            "product_code" => "prod_QGSisdsXztA3Fj",
+            // "product_code" => "prod_QGSisdsXztA3Fj",
+            "product_code" => "prod_QaPvd15Di5rIsw",
             "name" => "150 connections",
             "description" => "Up to 150 connections",
         ]);
         $jobseeker150connections->product_plans()->create([
-            "price_code" => "price_1PPvnGChe3vlhgX3mA9RUHi6",
+            // "price_code" => "price_1PPvnGChe3vlhgX3mA9RUHi6",
+            "price_code" => "price_1PjF5qHxBaMvMKXwe1LOTwjC",
             "price" => 22.50,
             "recurring" => "month",
             "mode" => "subscription",
@@ -146,12 +162,14 @@ class ProductSeeder extends Seeder
         ]);
 
         $jobseeker200connections = Product::create([
-            "product_code" => "prod_QGSjDZWoCIHgmE",
+            // "product_code" => "prod_QGSjDZWoCIHgmE",
+            "product_code" => "prod_QaPw6WPEAa0xDG",
             "name" => "200 connections",
             "description" => "Up to 200 connections",
         ]);
         $jobseeker200connections->product_plans()->create([
-            "price_code" => "price_1PPvoAChe3vlhgX3hlCNk73f",
+            // "price_code" => "price_1PPvoAChe3vlhgX3hlCNk73f",
+            "price_code" => "price_1PjF6HHxBaMvMKXw9yEaCMcg",
             "price" => 30.00,
             'recurring' => "month",
             "mode" => "subscription",
@@ -162,12 +180,14 @@ class ProductSeeder extends Seeder
         ]);
 
         $jobseeker250connections = Product::create([
-            "product_code" => "prod_QGSkUK3J5p5yM4",
+            // "product_code" => "prod_QGSkUK3J5p5yM4",
+            "product_code" => "prod_QaPw6Pv5IX8BWp",
             "name" => "250 connections",
             "description" => "Up to 250 connections",
         ]);
         $jobseeker250connections->product_plans()->create([
-            'price_code' => 'price_1PPvopChe3vlhgX3BGkq2LPf',
+            // 'price_code' => 'price_1PPvopChe3vlhgX3BGkq2LPf',
+            'price_code' => 'price_1PjF6rHxBaMvMKXw6kbZt1ER',
             'price' => 37.50,
             'recurring' => "month",
             "mode" => "subscription",
@@ -178,12 +198,14 @@ class ProductSeeder extends Seeder
         ]);
 
         $jobseeker300connections = Product::create([
-            "product_code" => "prod_QGSk1p9K1freu6",
+            // "product_code" => "prod_QGSk1p9K1freu6",
+            "product_code" => "prod_QaPxpGK3bse1oG",
             "name" => "300 connections",
             "description" => "Up to 300 connections",
         ]);
         $jobseeker300connections->product_plans()->create([
-            'price_code' => 'price_1PPvpLChe3vlhgX391uV1lYx',
+            // 'price_code' => 'price_1PPvpLChe3vlhgX391uV1lYx',
+            'price_code' => 'price_1PjF7KHxBaMvMKXwghzHDmSS',
             'price' => 45.00,
             'recurring' => "month",
             "mode" => "subscription",
@@ -195,12 +217,14 @@ class ProductSeeder extends Seeder
 
         // employer
         $posting10 = Product::create([
-            "product_code" => "prod_QEaCk0aloAns2e",
+            // "product_code" => "prod_QEaCk0aloAns2e",
+            "product_code" => "prod_QaPiHUp1AJO7Hy",
             "name" => "Posting (10 applicants)",
             "description" => "Ideal for small businesses or specialized roles, this plan ensures you receive a manageable number of high-quality applicants. Pay only for the applicants you receive, making it a cost-effective solution for targeted recruitment.",
         ]);
         $posting10->product_plans()->create([
-            "price_code" => "price_1PO72CChe3vlhgX33HU8Nvv9",
+            // "price_code" => "price_1PO72CChe3vlhgX33HU8Nvv9",
+            "price_code" => "price_1PjEsxHxBaMvMKXwKNZWR3Js",
             "price" => 499.00,
             "recurring" => "month",
             "mode" => "subscription",
@@ -212,12 +236,14 @@ class ProductSeeder extends Seeder
         ]);
 
         $posting30 = Product::create([
-            "product_code" => "prod_QEaDseKEl7WejA",
+            // "product_code" => "prod_QEaDseKEl7WejA",
+            "product_code" => "prod_QaPjy6g0FBZrDx",
             "name" => "Posting (30 applicants)",
             "description" => "Perfect for growing businesses with moderate hiring needs, this plan offers a balance between cost and quantity. Receive a steady stream of candidates without overspending, ensuring you find the right fit for your team.",
         ]);
         $posting30->product_plans()->create([
-            "price_code" => "price_1PO732Che3vlhgX3Eta28c13",
+            // "price_code" => "price_1PO732Che3vlhgX3Eta28c13",
+            "price_code" => "price_1PjEtvHxBaMvMKXwPSALxvo6",
             "price" => 999.00,
             "recurring" => "month",
             "mode" => "subscription",
@@ -228,12 +254,14 @@ class ProductSeeder extends Seeder
         ]);
 
         $posting50 = Product::create([
-            "product_code" => "prod_QEaErbv658hhDw",
+            // "product_code" => "prod_QEaErbv658hhDw",
+            "product_code" => "prod_QaPj86bVzzDBOk",
             "name" => "Posting (50 applicants)",
             "description" => "Best suited for larger organizations or high-volume recruitment, this plan provides a substantial number of applicants. Invest in this plan to efficiently meet your hiring demands and access a broad pool of potential candidates."
         ]);
         $posting50->product_plans()->create([
-            "price_code" => "price_1PO73fChe3vlhgX3eSqpcmlf",
+            // "price_code" => "price_1PO73fChe3vlhgX3eSqpcmlf",
+            "price_code" => "price_1PjEuTHxBaMvMKXwGtC1SdyB",
             "price" =>  1499.00,
             "recurring" => "month",
             "mode" => "subscription",
@@ -244,7 +272,8 @@ class ProductSeeder extends Seeder
         ]);
 
         $singlePlan = Product::create([
-            "product_code" => "prod_Q9MH6RlQ6syLeL",
+            // "product_code" => "prod_Q9MH6RlQ6syLeL",
+            "product_code" => "prod_QaPX0Dk2riP8hn",
             "name" => "Single Plan",
             "description" => "1 post and 10 applicants per post per month. Perfect for small businesses or occasional hiring needs.",
         ]);
@@ -252,7 +281,8 @@ class ProductSeeder extends Seeder
         $singleProductPlan = [
             [
                 "product_id" => $singlePlan->id,
-                "price_code" => "price_1PPvJSChe3vlhgX3na97xNFU",
+                // "price_code" => "price_1PPvJSChe3vlhgX3na97xNFU",
+                "price_code" => "price_1PjEhyHxBaMvMKXwpXuwkh3a",
                 "price" => 20.00,
                 "mode" => "subscription",
                 "unit_label" => "employer",
@@ -266,7 +296,8 @@ class ProductSeeder extends Seeder
             ],
             [
                 "product_id" => $singlePlan->id,
-                "price_code" => "price_1PPvJlChe3vlhgX3Us9ZTSjU",
+                // "price_code" => "price_1PPvJlChe3vlhgX3Us9ZTSjU",
+                "price_code" => "price_1PjEjhHxBaMvMKXwvC6JxQFH",
                 "price" => 117.00,
                 "mode" => "subscription",
                 "unit_label" => "employer",
@@ -283,14 +314,16 @@ class ProductSeeder extends Seeder
         // $singlePlan->product_plans()->insert($singleProductPlan);
         ProductPlan::insert($singleProductPlan);
         $familyPlan = Product::create([
-            "product_code" => "prod_Q9MJloheLiCl2U",
+            // "product_code" => "prod_Q9MJloheLiCl2U",
+            "product_code" => "prod_QaPYuhq1QCoxXB",
             "name" => "Family Plan",
             "description" => "3 posts and 15 applicants per post per month. Ideal for moderate hiring demands.",
         ]);
         $familyProductPlan = [
             [
                 "product_id" => $familyPlan->id,
-                "price_code" => "price_1PPvLIChe3vlhgX3cRLSZmIK",
+                // "price_code" => "price_1PPvLIChe3vlhgX3cRLSZmIK",
+                "price_code" => "price_1PjEjJHxBaMvMKXw5KyPNsYA",
                 "price" => 49.00,
                 "mode" => "subscription",
                 "unit_label" => "employer",
@@ -304,7 +337,8 @@ class ProductSeeder extends Seeder
             ],
             [
                 "product_id" => $familyPlan->id,
-                "price_code" => "price_1PPvLZChe3vlhgX3yDexWY88",
+                // "price_code" => "price_1PPvLZChe3vlhgX3yDexWY88",
+                "price_code" => "price_1PjEjJHxBaMvMKXwHEkCHt79",
                 "price" => 298.00,
                 "mode" => "subscription",
                 "unit_label" => "employer",
@@ -321,7 +355,8 @@ class ProductSeeder extends Seeder
         ProductPlan::insert($familyProductPlan);
 
         $extendedPlan = Product::create([
-            "product_code" => "prod_Q9MMMH7NaIMZaW",
+            // "product_code" => "prod_Q9MMMH7NaIMZaW",
+            "product_code" => "prod_QaPazjQ7JSwgKj",
             "name" => "Extended Plan",
             "description" => "10 job posts with up to 20 applicants per post per month. Best for larger businesses with significant recruitment needs.",
         ]);
@@ -329,7 +364,8 @@ class ProductSeeder extends Seeder
         $extendedProductPlan = [
             [
                 "product_id" => $extendedPlan->id,
-                "price_code" => "price_1PPvMSChe3vlhgX3MRjalT36",
+                // "price_code" => "price_1PPvMSChe3vlhgX3MRjalT36",
+                "price_code" => "price_1PjElNHxBaMvMKXwbLL5WKXJ",
                 "price" => 69.00,
                 "mode" => "subscription",
                 "unit_label" => "employer",
@@ -343,7 +379,8 @@ class ProductSeeder extends Seeder
             ],
             [
                 "product_id" => $extendedPlan->id,
-                "price_code" => "price_1PPvMiChe3vlhgX3lVBA7ohc",
+                // "price_code" => "price_1PPvMiChe3vlhgX3lVBA7ohc",
+                "price_code" => "price_1PjElNHxBaMvMKXwtq0nxlCH",
                 "price" => 417.00,
                 "mode" => "subscription",
                 "unit_label" => "employer",
@@ -359,7 +396,8 @@ class ProductSeeder extends Seeder
         // $extendedPlan->product_plans()->insert($extendedProductPlan);
         ProductPlan::insert($extendedProductPlan);
         $company1 = Product::create([
-            "product_code" => "prod_Q9MOSJQlkICTyh",
+            // "product_code" => "prod_Q9MOSJQlkICTyh",
+            "product_code" => "prod_QaPdWVKksOAt5C",
             "name" => "Company Plan #1",
             "description" => "Includes 20 posts and 30 applicants per post per month. Ideal for growing businesses.",
         ]);
@@ -367,7 +405,8 @@ class ProductSeeder extends Seeder
         $company1ProductPlan = [
             [
                 "product_id" => $company1->id,
-                "price_code" => "price_1PPvNJChe3vlhgX3KAVRVH7s",
+                // "price_code" => "price_1PPvNJChe3vlhgX3KAVRVH7s",
+                "price_code" => "price_1PjEo4HxBaMvMKXw7VLyesIs",
                 "price" => 104.00,
                 "mode" => "subscription",
                 "unit_label" => "employer",
@@ -381,7 +420,8 @@ class ProductSeeder extends Seeder
             ],
             [
                 "product_id" => $company1->id,
-                "price_code" => "price_1PPvNSChe3vlhgX3Mvevupt6",
+                // "price_code" => "price_1PPvNSChe3vlhgX3Mvevupt6",
+                "price_code" => "price_1PjEpGHxBaMvMKXw0BsxknOE",
                 "price" => 627.00,
                 "recurring" => "year",
                 "mode" => "subscription",
@@ -398,7 +438,8 @@ class ProductSeeder extends Seeder
         $company1->product_plans()->insert($company1ProductPlan);
         ProductPlan::insert($company1ProductPlan);
         $company2 = Product::create([
-            "product_code" => "prod_Q9MPY7scX06iRZ",
+            // "product_code" => "prod_Q9MPY7scX06iRZ",
+            "product_code" => "prod_QaPhh5MDAuXRv7",
             "name" => "Company Plan #2",
             "description" => "Includes 50 posts and 50 applicants per post per month. Perfect for large-scale hiring.",
         ]);
@@ -407,7 +448,8 @@ class ProductSeeder extends Seeder
             [
 
                 "product_id" => $company2->id,
-                "price_code" => "price_1PPvQZChe3vlhgX3gZKZwEk7",
+                // "price_code" => "price_1PPvQZChe3vlhgX3gZKZwEk7",
+                "price_code" => "price_1PjEsCHxBaMvMKXwIQr3q4Qj",
                 "price" => 199.00,
                 "mode" => "subscription",
                 "unit_label" => "employer",
@@ -421,7 +463,8 @@ class ProductSeeder extends Seeder
             ],
             [
                 "product_id" => $company2->id,
-                "price_code" => "price_1PPvQnChe3vlhgX3wbzysQry",
+                // "price_code" => "price_1PPvQnChe3vlhgX3wbzysQry",
+                "price_code" => "price_1PjEsCHxBaMvMKXwCYaKTxlC",
                 "price" => 1194.00,
                 "mode" => "subscription",
                 "unit_label" => "employer",
