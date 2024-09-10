bb5f6828ef70d74de229e5bc7252253df0d53338
henryEguia <henry.eguia@kodakollectiv.com>
2024-09-10 13:21:20 +0800

    update checkout url

--- database/migrations/2023_08_11_032925_update_data_types_for_joblists.php
+++ database/migrations/2023_08_11_032925_update_data_types_for_joblists.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,10 +12,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('job_lists', function(Blueprint $table){
-            $table->boolean('is_vaccinated')->default(false)->change();
-            $table->boolean('can_commute')->default(false)->change();
-        });
+        // Schema::table('job_lists', function(Blueprint $table){
+        //     $table->boolean('is_vaccinated')->change();
+        //     $table->boolean('can_commute')->change();
+        // });
+		DB::statement('ALTER TABLE job_lists ALTER COLUMN 
+                  is_vaccinated TYPE boolean USING (is_vaccinated)::boolean');
+		DB::statement('ALTER TABLE job_lists ALTER COLUMN 
+                  can_commute TYPE boolean USING (can_commute)::boolean');
     }
 
     /**
